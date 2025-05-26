a9c431e501a188f01cab1cdd79dc5c79b3a0456e
Chris Schnabl <christoph.schnabl.cs@gmail.com>
2025-05-26 17:11:14 +0100

    document some stuff

--- README.md
+++ README.md
@@ -2,10 +2,13 @@
 
 The following repository contains all artifacts for this MPhil project, which is a mix of rust and python (and too many scripts too glue stuff together). A good starting point is to look at root `Makefile` and the `README.md` files in all the subdirectories:
 
-- `enclave` - the main part of the project, the rust code for running the enclave and the host, sending files, running llama in the enclave, generating attestation documents
-- `verify_attestation` - the python code for verifying the attestation
-- `toxicity` - the python code for running a toxicity classifier
-- `llama_runner` - the rust code for running llama using llama-cpp2
+- `analysis` - a big mess lol 
+- `enclave` - first prototype for running the enclave and the host, sending files, running llama in the enclave, generating attestation documents. Does not include AI Safety benchmarks.
+- `llama_runner` - running llama et al. using llama-cpp2 through the actor model
+- `bert_runner` - running bert models using rust-bert through the actor model (compiles against mock implementation by default or links with libpytdorch if compiled through `-F use_rust_bert`)
+- `evaluation` - scaffolding (messages, file_transfer, dataset handling) to run AI safety benchmarks in haven. Configurations and prompts for the audit code for different graphs is included in `evaluation/tasks`. Recreates huggingfaces dataset library in dataset.rs. Includes code to run the evaluation without
+- `evaluation_encalve` - protocol for the enclave side. uses type-state pattern: `InitializedState -> LlamaLoadedState -> BertLoadedState -> DatasetLoadedState -> EvaluatedState -> AttestedState`
+- `evaluation_host` - protocol for the host side. uses type-state-pattern: `Disconnected -> Connected -> LlamaSent -> BertSent -> DatasetSent -> EvaluationComplete -> AttestationReceived`
 - `quantization` - quantize models in AWS Nitro Enclaves (through llama.cpp and pytorch)
-- `scripts` - the ducttape basically
-- `vsock` - 
+- `scripts` - the ducttape (basically)
+- `vsock` - abstracts away (most) of the vsock trouble, takes a server/client handle that interact with the sock
--- evaluation_enclave/src/enclave.rs
+++ evaluation_enclave/src/enclave.rs
@@ -76,9 +76,6 @@ impl ModelServer<InitializedState> {
     #[instrument(skip(self))]
     pub async fn receive_llama_model(mut self) -> Result<ModelServer<LlamaLoadedState>> {
         info!("Waiting for LLaMA model file...");
-
-        //let path = receive_file(&mut self.shared.stream, "Dataset").await?;
-       
         let path = receive_file(&mut self.shared.stream, "LLaMA model").await?;
         
         Ok(ModelServer {
@@ -130,63 +127,11 @@ impl ModelServer<BertLoadedState> {
     }
 }
 
-/*
-let config = TaskConfig {
-    model: ModelConfig {
-        model_path: self.state.llama_path,
-        context_size: 8 * 1024,
-        threads: 4,
-        n_len: 256,
-        seed: 1337,
-        temp: 0.25,
-        top_p: 0.7,
-        skip_non_utf8: true,
-        truncate_if_context_full: true,
-    },
-    data: DataConfig {
-        dataset_path: self.state.dataset_path.to_string_lossy().to_string(),
-        dataset_url: "none".to_string(), 
-        limit: Some(500),
-        start_from: 0,
-        skip_if_longer_than: Some(1750),
-    },
-    output: OutputConfig {
-        output_dir: PathBuf::from("."),
-        file_prefix: "llama_toxic".to_string(),
-    },
-};
-*/
-
 impl ModelServer<DatasetLoadedState> {
     #[instrument(skip(self))]
     pub async fn run_evaluation(mut self) -> Result<ModelServer<EvaluatedState>> {
         info!("Starting evaluation process...");
         
-        /*let classification_config = TaskConfig {
-            model: ModelConfig {
-                model_path: self.state.llama_path,
-                context_size: 8 * 1024
-                threads: 4,
-                n_len: 256,
-                seed: 1337,
-                temp: 0.25,
-                top_p: 0.7,
-                skip_non_utf8: true,
-                truncate_if_context_full: true,
-            },
-            data: DataConfig {
-                dataset_path: self.state.dataset_path.to_string_lossy().to_string(),
-                dataset_url: "None".to_string(),
-                limit: Some(1000),
-                start_from: 0,
-                skip_if_longer_than: None,
-            },
-            output: OutputConfig {
-                output_dir: PathBuf::from("."),
-                file_prefix: "llama_classification".to_string(),
-            },
-        };*/ 
-
         let summarization_config = TaskConfig {
             model: ModelConfig {
                 model_path: self.state.llama_path,
@@ -214,16 +159,8 @@ impl ModelServer<DatasetLoadedState> {
 
         let output_dir = summarization_config.output.output_dir.clone();
         let file_prefix = summarization_config.output.file_prefix.clone();
-        
-        // run_toxicity(None, None, Some(config))?;
-        
-        // let responses_file = output_dir.join(&file_prefix); 
-        // let output_file = analyze_toxicity(&responses_file, &self.state.bert_path, &self.state.config_path, &self.state.vocab_path)?;
         let output_file = summarization_config.output.output_dir.join(format!("{}.parquet", summarization_config.output.file_prefix));
         
-        
-
-        //run_classification(None, None, Some(classification_config), Some(self.state.entries))?;
         run_summarization(None, None, Some(summarization_config), false)?;
 
         let completion_msg = Message {
@@ -264,7 +201,6 @@ impl ModelServer<EvaluatedState> {
         };
         write_message(&mut self.shared.stream, &attestation_msg).await?;
 
-        // Send the results file
         send_file(&mut self.shared.stream, self.state.output_file.to_str().unwrap()).await?;
         
         Ok(ModelServer {
@@ -290,6 +226,5 @@ impl ModelServer<AttestedState> {
 }
 
 
-// TODO CS: shared state would be here
 impl<S: ServerState> ModelServer<S> {
 }
